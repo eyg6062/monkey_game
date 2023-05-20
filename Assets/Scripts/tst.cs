a265913a36740fab6a1cdfab9ca01696206cbb39
eyg6062 <eyg6062@rit.edu>
2023-05-20 17:05:24 -0400

    added static sprites

---
+++ Assets/Scripts/tst.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DWMidi = Melanchall.DryWetMidi;
+
+public class tst : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
